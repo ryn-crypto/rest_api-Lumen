38e5fb447d519460315c1f7e37cfdce05d598392
Riyan First Tiyanto <riyanfirst13@gmail.com>
2022-04-21 21:10:56 +0700

    menambahkan respose pada controler

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function responseHasil($code, $status, $data)
+    {
+        return \response()-> json([
+            'code'      => $code,
+            'status'    => $status,
+            'data'      => $data 
+        ]);
+    }
 }
